0ca979f91da74d5db36c2a17c3db1c852b6a2f1e
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2020-12-16 19:15:41 -0500

    Update docker-publish.sh

--- docker-publish.sh
+++ docker-publish.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest --push .
+docker buildx build --platform linux/ppc64le,linux/s390x,linux/386,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest --push .
