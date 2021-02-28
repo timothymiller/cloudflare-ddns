86c935dea7d12e6882ecba2c6d27e6dc56c2aeae
Timothy Miller <tim.miller@preparesoftware.com>
2021-02-28 12:06:38 -0500

    🐳 CI Multi-Arch Docker Builds 🗄️ Organized scripts

---
+++ scripts/docker-build-all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker buildx build --platform  linux/ppc64le,linux/s390x,linux/386,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest ../
+# TODO: Support linux/riscv64
---
+++ scripts/docker-build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build --platform linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest ../
