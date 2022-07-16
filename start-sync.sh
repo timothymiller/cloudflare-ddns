65b63684723cad9490da8aa9c578a10f6c3a2f67
notssh <51744301+notssh@users.noreply.github.com>
2022-07-17 00:15:03 +0300

    #74 Fix
    https://github.com/timothymiller/cloudflare-ddns/issues/74 fix

--- start-sync.sh
+++ start-sync.sh
@@ -4,7 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 python3 -m venv venv
 source ./venv/bin/activate
 
-set -o pipefail; pip install -r requirements.txt | { grep -v "already satisfied" || :; }
-
 cd $DIR
+set -o pipefail; pip install -r requirements.txt | { grep -v "already satisfied" || :; }
 python3 cloudflare-ddns.py
