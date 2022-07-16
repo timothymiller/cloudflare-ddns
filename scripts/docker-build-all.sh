5e62372192da5c536759431a6834417364308ead
notssh <51744301+notssh@users.noreply.github.com>
2022-07-17 00:08:04 +0300

    Delete scripts directory

--- scripts/docker-build-all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker buildx build --platform  linux/ppc64le,linux/s390x,linux/386,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest ../
-# TODO: Support linux/riscv64
--- scripts/docker-build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build --platform linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest ../
