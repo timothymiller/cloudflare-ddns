d8fee109b4d83b41c4d0d7fe096fa7a46eefa3be
Steven Schmidt <steven@thisismynameok.com>
2026-03-07 22:13:51 +1100

    Update build/deploy scripts to reference my repo

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 BASH_DIR=$(dirname $(realpath "${BASH_SOURCE}"))
-docker build --platform linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest ${BASH_DIR}/../
+docker build --platform linux/amd64 --tag thisismynameok/cloudflare-ddns:latest ${BASH_DIR}/../
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run timothyjmiller/cloudflare-ddns:latest
+docker run thisismynameok/cloudflare-ddns:latest
